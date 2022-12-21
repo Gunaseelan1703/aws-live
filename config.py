a189665a7967c0d5fc0b005ed571585e194d018c
Gunaseelan <gunaseelann-wg17@student.tarc.edu.my>
2022-12-21 17:36:49 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-customhost = "employee.c721kcjdpsrz.us-east-1.rds.amazonaws.com"
+customhost = "employee.ccoluovpdfec.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "sootzanjian-employee"
+custombucket = "gunaseelan-employee"
 customregion = "us-east-1"
